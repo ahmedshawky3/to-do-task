6bff6d2ba3825bfd96a55ec72fe904d713711d8d
ahmedshawky3 <ahmed.shawkiy123@gmail.com>
2025-04-09 23:20:29 +0200

    Update axios baseUR

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'https://to-do-task-backend.onrender.com/api',
+    baseURL: `${process.env.REACT_APP_API_URL}/api` || 'https://to-do-task-backend.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
